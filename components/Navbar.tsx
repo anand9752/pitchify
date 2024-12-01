c092df2f75f2f52691e55bd15cfb029993043d34
Anand Patidar <patidaranand9752@gmail.com>
2024-12-02 00:01:12 +0530

    navbar correct

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,13 @@ const Navbar = async () => {
         <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                    {/* <Image src="/logo.png" alt="logo" width={144} height={30} /> */}
+
+                  <div className='w-[144px] h-[30]' >
+                     <span className='font-bold text-3xl  text-primary'>Pi</span><span className='font-bold text-3xl'>tchify</span>
+                  </div>
+
+                    
                 </Link>
 
                 <div className='flex items-center gap-5 text-black'>
