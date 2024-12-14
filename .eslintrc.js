63686e225c5d1cb9dbefc69a4874d39dbce48a59
Anand Patidar <patidaranand9752@gmail.com>
2024-12-14 22:10:15 +0530

    last commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   extends: ["next", "next/core-web-vitals", "next/typescript"],
   rules: {
     "@next/next/no-img-element": "off", // Disables warnings for <img> instead of <Image>
-    // "@typescript-eslint/no-unused-vars": [
-    //   "error",
-    //   { argsIgnorePattern: "^_", varsIgnorePattern: "^ignored" }
-    // ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^ignored" }
+    ],
     "@typescript-eslint/no-explicit-any": "warn", // Allows 'any' but shows a warning to encourage better typing
     "no-console": ["warn", { allow: ["warn", "error"] }], // Encourages limiting console statements
-    "react/react-in-jsx-scope": "off" // Not needed for Next.js as React is automatically in scope
+     "react/react-in-jsx-scope": "off", // Not needed for Next.js as React is automatically in scope
+    "@typescript-eslint/ban-ts-comment": "off" // Add this line to ignore TypeScript comments
   }
 };
