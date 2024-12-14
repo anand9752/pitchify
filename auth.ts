1714a3a4a1bba446b93739e6ad8a7376bed273ed
Anand Patidar <patidaranand9752@gmail.com>
2024-12-14 20:47:59 +0530

    today commit

--- auth.ts
+++ auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/quries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/quries";
 
-export default NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
     async signIn({
